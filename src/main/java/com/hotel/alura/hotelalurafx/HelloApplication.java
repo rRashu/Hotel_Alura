2e57dae10004f61c14b58b70422490dc681324ea
Robinson <robinsonstalinr@gmail.com>
2023-04-28 12:00:46 -0500

    Inicio - Login Guapo

---
+++ src/main/java/com/hotel/alura/hotelalurafx/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.hotel.alura.hotelalurafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader;
+        fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Hello!");
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("hasta la proxima");
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
